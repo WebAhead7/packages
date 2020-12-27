5d5cf302ea18d0911fd690c5873bc7f760c6cb85
Alaa Bashiyi <alaabashiyi@Alaas-iMac.local>
2020-12-27 12:47:49 +0200

    sign up ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import Signup from "./screens/Signup";
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      <Login />
+      <Signup />
     </div>
   );
 }
---
+++ src/screens/Signup/index.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import {
+  Button,
+  TextField,
+  Slider,
+  Typography,
+  Input,
+  InputAdornment,
+  IconButton,
+  InputLabel,
+  FormControl,
+} from "@material-ui/core";
+// import AccountCircle from "@material-ui/icons/AccountCircle";
+import styles from "./styles";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+
+const Signup = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handlerInputs = (event, inputName) => {
+    setValues({ ...values, [inputName]: event.target.value });
+  };
+
+  return (
+    <div>
+      <div style={styles.container}>
+        <p style={styles.header}>Sign up</p>
+        <FormControl>
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <Input
+            type="email"
+            label="Email"
+            id="email"
+            onChange={(e) => handlerInputs(e, "email")}
+            value={values.email}
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <Input
+            id="password"
+            type={"password"}
+            value={values.password}
+            onChange={(e) => handlerInputs(e, "password")}
+          />
+        </FormControl>
+      </div>
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ marginTop: 50 }}
+        onClick={() => console.log(values)}
+      >
+        Login
+      </Button>
+    </div>
+  );
+};
+
+export default Signup;
